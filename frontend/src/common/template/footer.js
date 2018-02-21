3d9c35e304cecd425c0ff8325bfc0e93a9f95fd6
joaldodecarvalho <joaldocarvalho14@gmail.com>
2018-02-20 22:34:30 -0300

    Initial commit frontend

---
+++ frontend/src/common/template/footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href='https://github.com/joaldodecarvalho' target='_blank'> JC</a>
+        </strong>
+    </footer>
+)
