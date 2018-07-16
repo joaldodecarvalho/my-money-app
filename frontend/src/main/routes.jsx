b51dd5f2df0cce61d3f47ca1844e81b80d3b8edb
joaldodecarvalho <joaldocarvalho14@gmail.com>
2018-07-16 15:01:17 -0300

    adicionando login

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { IndexRoute, Router, Route, Redirect, hashHistory } from 'react-router'
 
-import App from './app'
+import AuthOrApp from './authOrApp'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
